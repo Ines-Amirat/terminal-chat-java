c11384e1dbc672a2761e8d0109a58bc995e62e1d
Ines Amirat <inesamirat20@gmail.com>
2025-04-16 23:54:07 +0100

    last push

--- src/main/java/org/chatApp/Client.java
+++ src/main/java/org/chatApp/Client.java
@@ -1,24 +1,74 @@
 package org.chatApp;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            Socket clientSocket = new Socket("127.0.0.1", 7777);
-            String message = scanner.nextLine();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
-            out.println(message);
+    private Socket clientSocket;
+    private  String clientName;
+    private  String serverIp;
+    private  int port ;
+    private PrintWriter out ;
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
+    public Client(String clientName,String serverIp , int port) throws IOException {
+        this.clientSocket = new Socket(serverIp,port);;
+        this.clientName = clientName;
+        this.serverIp = serverIp;
+        this.port = port;
+        this.out =  new PrintWriter(clientSocket.getOutputStream(), true);
+        this.out.println(this.clientName);
+
     }
+            public void threadReceiver(){
+                Thread client =  new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            InputStream input = clientSocket.getInputStream();
+                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
+                            String msg = bufferedReader.readLine();
+                            System.out.println(msg);
 
+                            while((msg = bufferedReader.readLine()) != null){
+                                System.out.println(msg);
+                            }
+                            System.out.println("Connexion finished by the server");
+                            clientSocket.close();
+
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                });
+                client.start();
+            }
+            public void sendMessage(){
+                Scanner scanner = new Scanner(System.in);
+
+                //loop never stop
+                while (true) {
+                    String message = scanner.nextLine();
+                    out.println(message);
+
+                }
+            }
+
+
+
+    public static void main(String[] args) {
+        String clientName = args[0];
+        String serverIp = args[1] ;
+        int port = Integer.parseInt(args[2]);
+        try {
+            Client user  = new Client(clientName ,serverIp ,port);
+            user.threadReceiver();
+            user.sendMessage();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
 }
--- src/main/java/org/chatApp/Server.java
+++ src/main/java/org/chatApp/Server.java
@@ -6,26 +6,81 @@ import java.lang.Thread;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import org.chatApp.Client;
 
 public class Server {
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new  ServerSocket(7777 );
+            ServerSocket serverSocket = new  ServerSocket(7778 );
             System.out.println("Server Socket listen");
-            List<Socket> clients = new ArrayList<>();
+            Map<Socket,PrintWriter> writers = new HashMap<>();
+            Map<Socket,String> clients = new HashMap<>();
+
+
             while(true){
                 Socket socket = serverSocket.accept();
-                clients.add(socket);
-                System.out.println("new client connected");
+                final  Socket currentSocket = socket;
+
+                PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+
                 Thread client =  new Thread(new Runnable() {
                     @Override
                     public void run() {
                         try {
-                            InputStream  input = socket.getInputStream();
-                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
-                            String msg = bufferedReader.readLine();
-                            System.out.println(msg);
+                            InputStream input = currentSocket.getInputStream();
+                            BufferedReader bufferedReader= new BufferedReader(new InputStreamReader(input));
+                            String clientName = bufferedReader.readLine();
+
+                            clients.put(currentSocket,clientName);
+                            writers.put(currentSocket,out);
+
+                            for(Socket s : clients.keySet()){
+                                if(s != currentSocket){
+                                    PrintWriter out = writers.get(s);
+                                    out.println(clientName + " connected");
+                                }
+                            }
+
+                            for(Socket s : clients.keySet()) {
+                                if(s != currentSocket) {
+                                    String name = clients.get(s);
+                                    out.println(name + " is already connected");
+                                }
+                            }
+
+                            while(true) {
+                                String msg = bufferedReader.readLine();
+                                if(msg ==null || msg.equals("quit")){
+
+                                    for(Socket s : clients.keySet()) {
+                                        if(s != currentSocket) {
+                                            PrintWriter out = writers.get(s);
+                                            out.println(clientName + " quit the chat");
+                                        }
+                                    }
+                                     clients.remove(currentSocket);
+                                     writers.remove(currentSocket);
+                                     currentSocket.close();
+
+                                     break;
+
+                                }
+
+                                else {
+                                    System.out.println(msg);
+                                    for(Socket s : clients.keySet()){
+                                        if(s != currentSocket){
+                                            PrintWriter out = writers.get(s);
+                                            out.println(clientName + ":" + msg);
+                                        }
+                                    }
+                                }
+                            }
 
                         } catch (IOException e) {
                             throw new RuntimeException(e);
